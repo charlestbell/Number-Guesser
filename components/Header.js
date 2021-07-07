886cb609496ac5625587c67a3f2979fbde96d717
C.T. Bell <charlestbell@gmail.com>
2021-07-07 12:03:08 -0400

    Refactoring platform specific styles in Header

--- components/Header.js
+++ components/Header.js
@@ -5,23 +5,39 @@ import TitleText from '../components/TitleText';
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
 
-    backgroundColor: Platform.OS == 'android' ? colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: Platform.OS == 'android' ? colors.primary : 'white',
     borderBottomColor: Platform.OS == 'ios' ? '#ccc' : 'transparent',
     borderBottomWidth: Platform.OS == 'ios' ? 1 : 0,
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: colors.primary,
   },
   headerTitle: {
     color: Platform.OS === 'ios' ? colors.primary : 'white',
